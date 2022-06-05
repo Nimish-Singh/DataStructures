7d89f255fce0b5d3bb51a09ec8836cfc06c47255
Nimish Singh <nimishs@thoughtworks.com>
2022-06-05 11:25:37 +0530

    Adds the problem to find the number of islands in a binary grid

---
+++ src/main/java/graph/NumberOfIslands.java
@@ -0,0 +1,42 @@
+package graph;
+
+// https://leetcode.com/problems/number-of-islands/
+public class NumberOfIslands {
+    public int numIslands(char[][] grid) {
+        int totalRows = grid.length;
+        int totalColumns = grid[0].length;
+        boolean[][] visited = new boolean[totalRows][totalColumns];
+        int islands = 0;
+
+        for (int row = 0; row < totalRows; row++) {
+            for (int column = 0; column < totalColumns; column++) {
+                if (visited[row][column]) {
+                    continue;
+                }
+                if (grid[row][column] == '0') {
+                    visited[row][column] = true;
+                    continue;
+                }
+
+                islands++;
+                visitDFS(grid, row, column, visited);
+            }
+        }
+        return islands;
+    }
+
+    private void visitDFS(char[][] grid, int row, int column, boolean[][] visited) {
+        if (row < 0 || row >= grid.length || column < 0 || column >= grid[0].length || visited[row][column])
+            return;
+
+        visited[row][column] = true;
+
+        if (grid[row][column] == '0')
+            return;
+
+        visitDFS(grid, row - 1, column, visited);
+        visitDFS(grid, row + 1, column, visited);
+        visitDFS(grid, row, column - 1, visited);
+        visitDFS(grid, row, column + 1, visited);
+    }
+}
---
+++ src/test/java/graph/NumberOfIslandsTest.java
@@ -0,0 +1,45 @@
+package graph;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class NumberOfIslandsTest {
+    private NumberOfIslands noi = new NumberOfIslands();
+    private char[][] grid;
+
+    @Test
+    public void sampleInput1() {
+        grid = new char[][]{{'1', '1', '1', '1', '0'},
+                            {'1', '1', '0', '1', '0'},
+                            {'1', '1', '0', '0', '0'},
+                            {'0', '0', '0', '0', '0'}};
+
+        assertEquals(1, noi.numIslands(grid));
+    }
+
+    @Test
+    public void sampleInput2() {
+        grid = new char[][]{{'1', '1', '0', '0', '0'},
+                            {'1', '1', '0', '0', '0'},
+                            {'0', '0', '1', '0', '0'},
+                            {'0', '0', '0', '1', '1'}};
+
+        assertEquals(3, noi.numIslands(grid));
+    }
+
+    @Test
+    public void sampleInput3() {
+        grid = new char[][]{{'0', '0', '0', '0', '0'}};
+
+        assertEquals(0, noi.numIslands(grid));
+    }
+
+    @Test
+    public void sampleInput4() {
+        grid = new char[][]{{'1', '0'},
+                            {'0', '1'}};
+
+        assertEquals(2, noi.numIslands(grid));
+    }
+}
